546180b9e1d9d8bf5b6704914fac095689e82c10
MaxKits <maxim.kitsenko.r31@gmail.com>
2016-04-21 02:50:06 +0300

    Empty project and tests added

---
+++ src/MaxKitsenko/MaxKitsenko.Voeethos/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxKitsenko.Voeethos
+{
+    public class Class1
+    {
+    }
+}
