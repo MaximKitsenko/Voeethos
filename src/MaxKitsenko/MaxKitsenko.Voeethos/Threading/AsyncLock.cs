7a854160ea924e1bb60ee25710961915c518201f
MaxKits <maxim.kitsenko.r31@gmail.com>
2016-04-21 03:34:59 +0300

    AsyncLock added

---
+++ src/MaxKitsenko/MaxKitsenko.Voeethos/Threading/AsyncLock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MaxKitsenko.Voeethos.Threading
+{
+	public class AsyncLock
+	{
+		private readonly Task< Releaser > _releaser;
+		internal readonly AsyncSemaphore _semaphore;
+
+		public AsyncLock()
+		{
+			this._semaphore = new AsyncSemaphore( 1 );
+			this._releaser = Task.FromResult( new Releaser( this ) );
+		}
+
+		public Task< Releaser > LockAsync()
+		{
+			var wait = this._semaphore.WaitAsync();
+			return wait.IsCompleted ? this._releaser :
+				wait.ContinueWith( ( _, state ) => new Releaser( ( AsyncLock )state ),
+					this, CancellationToken.None,
+					TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default );
+		}
+
+	}
+
+	public struct Releaser: IDisposable
+	{
+		private readonly AsyncLock _toRelease;
+
+		internal Releaser( AsyncLock toRelease )
+		{
+			this._toRelease = toRelease;
+		}
+
+		public void Dispose()
+		{
+			if( this._toRelease != null )
+				this._toRelease._semaphore.Release();
+		}
+	}
+}
---
+++ src/MaxKitsenko/MaxKitsenko.Voeethos/Threading/AsyncSemaphore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MaxKitsenko.Voeethos.Threading
+{
+	public class AsyncSemaphore
+	{
+		private static readonly Task _completed = Task.FromResult( true );
+		private readonly Queue< TaskCompletionSource< bool > > _waiters = new Queue< TaskCompletionSource< bool > >();
+		private int _currentCount;
+
+		public AsyncSemaphore( int initialCount )
+		{
+			if( initialCount < 0 )
+				throw new ArgumentOutOfRangeException( "initialCount" );
+			this._currentCount = initialCount;
+		}
+
+		public void Release()
+		{
+			TaskCompletionSource< bool > toRelease = null;
+			lock( this._waiters )
+			{
+				if( this._waiters.Count > 0 )
+					toRelease = this._waiters.Dequeue();
+				else
+					++this._currentCount;
+			}
+			if( toRelease != null )
+				toRelease.SetResult( true );
+		}
+
+		public Task WaitAsync()
+		{
+			lock( this._waiters )
+			{
+				if( this._currentCount > 0 )
+				{
+					--this._currentCount;
+					return _completed;
+				}
+				var waiter = new TaskCompletionSource< bool >();
+				this._waiters.Enqueue( waiter );
+				return waiter.Task;
+			}
+		}
+	}
+}
